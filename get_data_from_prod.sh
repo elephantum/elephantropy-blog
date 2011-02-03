670bfe306eb520cb66151197f10b4c0d28f0c50e
Andrey Tatarinov <elephantum@yandex.ru>
2011-02-03 01:41:19 +0000

    fix port

--- get_data_from_prod.sh
+++ get_data_from_prod.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+rm -rf tmp
 mkdir -p tmp
 cd tmp
 appcfg.py download_data --application=elephantropy-blog --url=http://elephantropy-blog.appspot.com/_ah/remote_api --filename=1.txt
-appcfg.py upload_data --application=elephantropy-blog --url=http://localhost:8082/_ah/remote_api --filename=1.txt
+appcfg.py upload_data --application=elephantropy-blog --url=http://localhost:8080/_ah/remote_api --filename=1.txt
 cd ..
